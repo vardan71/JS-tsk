90a54e44787865b845016174484a91246ac1ee34
vardan71 <vardanhakobyan7771@gmail.com>
2022-10-19 18:21:32 +0400

    task3

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-let num = [1, 2, 3, 2, 3, 4, 4, 5, 5, 1];
+const num1 = [1, 2, 3, 4, 6, 60];
+const num2 = [60, 7, 8, 9, 5, 11, 12, 13, 45, 6, 1];
+let result = [];
 
-for (let i = 0; i < num.length; i++) {
-    for (let j = 1; j < num.length; j++) {
-        if (num[i] == num[i + j]) {
-            num.splice(i + j, 1);
+for (let i = 0; i < num1.length; i++) {
+    for (let j = 0; j < num2.length; j++) {
+        if (num1[i] == num2[j]) {
+            result.push(num2[j]);
         }
     }
 }
 
-console.log(num);
+console.log(result);
