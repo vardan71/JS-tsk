d496449a6e75e7bd62405ac344ffa23e5532b62c
vardan71 <vardanhakobyan7771@gmail.com>
2022-10-19 18:41:39 +0400

    tasks

--- app.js
+++ app.js
@@ -1,7 +1,40 @@
+// task1
+
+let num = [2, 3, 8, 1, 6];
+let c;
+
+for (let i = 0; i < num.length; i++) {
+    for (let j = 0; j < num.length; j++) {
+        if (num[j] > num[j + 1]) {
+            c = num[j];
+            num[j] = num[j + 1];
+            num[j + 1] = c;
+        }
+    }
+}
+
+console.log(num);
+
+// task 2
+
+let nums = [1, 2, 3, 2, 3, 4, 4, 5, 5, 1];
+
+for (let i = 0; i < nums.length; i++) {
+
+    for (let j = 1; j < nums.length; j++) {
+        if (nums[i] == nums[i + j]) {
+            nums.splice(i + j, 1);
+        }
+    }
+}
+
+// task3
+
 const num1 = [1, 2, 3, 4, 6, 60];
 const num2 = [60, 7, 8, 9, 5, 11, 12, 13, 45, 6, 1];
 let result = [];
 
+function res (){
 for (let i = 0; i < num1.length; i++) {
     for (let j = 0; j < num2.length; j++) {
         if (num1[i] == num2[j]) {
@@ -9,5 +42,8 @@ for (let i = 0; i < num1.length; i++) {
         }
     }
 }
+
+}
+
 
 console.log(result);
