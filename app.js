ac0c86e2abb7e179755f518faa522a81066d0300
vardan71 <vardanhakobyan7771@gmail.com>
2022-10-19 18:44:53 +0400

    tasks1

--- app.js
+++ app.js
@@ -32,18 +32,19 @@ for (let i = 0; i < nums.length; i++) {
 
 const num1 = [1, 2, 3, 4, 6, 60];
 const num2 = [60, 7, 8, 9, 5, 11, 12, 13, 45, 6, 1];
-let result = [];
+;
 
-function res (){
-for (let i = 0; i < num1.length; i++) {
-    for (let j = 0; j < num2.length; j++) {
-        if (num1[i] == num2[j]) {
-            result.push(num2[j]);
+function res(array1, array2) {
+    let result = []
+    for (let i = 0; i < array1.length; i++) {
+        for (let j = 0; j < array2.length; j++) {
+            if (array1[i] == array2[j]) {
+                result.push(array2[j]);
+            }
         }
     }
+    return result
 }
 
-}
 
-
-console.log(result);
+console.log(res(num1, num2));
