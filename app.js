5e865f4c8add186cb10e46890912f315f2407993
vardan71 <vardanhakobyan7771@gmail.com>
2022-10-19 18:16:43 +0400

    task1

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-app.js
+let num = [2, 3, 8, 1, 6];
+let c;
+
+for (let i = 0; i < num.length; i++) {
+    for (let j = 0; j < num.length; j++) {
+        if (num[j] > num[j + 1]) {
+            c = num[j];
+            num[j] = num[j + 1];
+            num[j + 1] = c;
+        }
+    }
+}
+
+console.log(num);
