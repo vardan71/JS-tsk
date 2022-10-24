7854ec337345421abeb76c558da9efde89d0f054
vardan71 <vardanhakobyan7771@gmail.com>
2022-10-24 18:14:39 +0400

    alltsks

--- app.js
+++ app.js
@@ -1,50 +1,53 @@
 // task1
 
-let num = [2, 3, 8, 1, 6];
+let num = [2, 3, 8, 1, 0];
 let c;
 
-for (let i = 0; i < num.length; i++) {
-    for (let j = 0; j < num.length; j++) {
-        if (num[j] > num[j + 1]) {
-            c = num[j];
-            num[j] = num[j + 1];
-            num[j + 1] = c;
+function result(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[j] > arr[j + 1]) {
+                c = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = c;
+            }
         }
     }
+    return arr;
 }
 
-console.log(num);
+console.log(result(num));
 
 // task 2
 
-let nums = [1, 2, 3, 2, 3, 4, 4, 5, 5, 1];
+const nums = [10, 20, 10, 30, 30, 30, 30, 20, 10];
 
-for (let i = 0; i < nums.length; i++) {
+function res() {
+    let obj = {}
 
-    for (let j = 1; j < nums.length; j++) {
-        if (nums[i] == nums[i + j]) {
-            nums.splice(i + j, 1);
-        }
+    for (let value of nums) {
+        obj[value] = 1;
     }
+    return Object.keys(obj);
 }
 
-// task3
+console.log(res(nums));
 
-const num1 = [1, 2, 3, 4, 6, 60];
-const num2 = [60, 7, 8, 9, 5, 11, 12, 13, 45, 6, 1];
-;
+// // task3
 
-function res(array1, array2) {
+const num1 = [1, 2, 3, 4, 6, 60, 60, 60,];
+const num2 = [60, 60, 60, 60, 7, 8, 9, 5, 11, 12, 13, 45, 6, 1];
+
+function res2(array1, array2) {
     let result = []
-    for (let i = 0; i < array1.length; i++) {
-        for (let j = 0; j < array2.length; j++) {
-            if (array1[i] == array2[j]) {
-                result.push(array2[j]);
+    for (let k = 0; k < array1.length; k++) {
+        if (array2.includes(array1[k])) {
+            if (!(result.includes(array1[k]))) {
+                result.push(array1[k]);
             }
         }
     }
     return result
 }
 
-
-console.log(res(num1, num2));
+console.log(res2(num1, num2));
